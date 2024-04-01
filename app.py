a9643442a10bc5b9bd3a9ad448fca57991a82c04
RezaDelvari <m.r.delvari@gmail.com>
2024-03-31 20:46:47 -0700

    update on the app

--- app.py
+++ app.py
@@ -3,21 +3,18 @@ import pandas as pd
 import pickle
 
 # Load the model
-# try:
-#     lgbm_model = joblib.load('lgbm_energy_prediction2.pkl')
-# except FileNotFoundError:
-#     st.error("Model file not found. Please make sure the model file is in the correct location.")
-# except Exception as e:
-#     st.error(f"Error loading the model: {e}")
-# with open('lgbm_energy_prediction3.pkl','rb') as file:
-#     lgbm_model = pickle.load(file)
-# lgbm = pickle.load(open('lgbm_energy_prediction4.pkl', 'rb'))
-with open('lgbm_energy_prediction4.pkl', 'rb') as pickle_in:
-    lgbm = pickle.load(pickle_in)
+@st.cache_data()
+def load_model():
+    with open('lgbm_energy_prediction4.pkl', 'rb') as pickle_in:
+        lgbm_model = pickle.load(pickle_in)
+    return lgbm_model
+
+lgbm_model = load_model()
+
 # Function to make predictions
 def make_prediction(input_data):
     try:
-        prediction = lgbm.predict(input_data)
+        prediction = lgbm_model.predict(input_data)
         return prediction
     except Exception as e:
         st.error(f"Error making prediction: {e}")
@@ -40,7 +37,7 @@ def main():
     st.sidebar.title('LinkedIn Barcode')
     st.sidebar.image('1.jpg', use_column_width=False, output_format='JPEG', width=300)
 
-    #Add "Created by Reza" with a hyperlink to your GitHub page at the bottom of the sidebar
+    # Add "Created by Reza" with a hyperlink to your GitHub page at the bottom of the sidebar
     st.sidebar.markdown(
         """  
         ---
@@ -59,15 +56,10 @@ def main():
     month = st.selectbox('Month', range(1, 13), index=0)
     day = st.selectbox('Day', range(1, 32), index=0)
     hour = st.slider('Hour', 0, 23, 0)
-
-    # Set installed_capacity to 0 if is_consumption is checked
     installed_capacity = 0 if is_consumption else st.number_input('Installed Capacity', value=1438.93017)
-
-    # Default values based on provided statistics
     default_total_precipitation_mean_f = 0.00008
     default_cloudcover_low_min_f = 0.28421
     default_direct_solar_radiation_mean_f = 146.44857
-
     total_precipitation_mean_f = st.number_input('Total Precipitation Mean (F)', value=default_total_precipitation_mean_f)
     cloudcover_low_min_f = st.number_input('Cloudcover Low Min (F)', value=default_cloudcover_low_min_f)
     direct_solar_radiation_mean_f = st.number_input('Direct Solar Radiation Mean (F)', value=default_direct_solar_radiation_mean_f)
@@ -76,7 +68,7 @@ def main():
     if st.button('Predict'):
         input_data = [[is_business, is_consumption, year, month, day, hour, installed_capacity, total_precipitation_mean_f, cloudcover_low_min_f, direct_solar_radiation_mean_f]]
         prediction = make_prediction(input_data)
-        if prediction:
+        if prediction is not None:
             if is_consumption:
                 st.write('Predicted energy consumption:', f'{prediction[0]:,.2f} kWh')  # Display result with kWh unit
             else:
@@ -84,4 +76,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
